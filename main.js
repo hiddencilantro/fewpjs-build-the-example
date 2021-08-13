0a28beaa6a9a1dcc7803e1ec98c5f2bdc58b49b3
hiddencilantro <stevekim1109@gmail.com>
2021-08-13 10:44:02 -0400

    Done.

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+let currentStatus = false
+
+const errorModal = document.querySelector('#modal')
+errorModal.setAttribute('class', 'hidden');
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const heart = document.querySelectorAll('.like-glyph');
+  for (let i = 0; i < heart.length; i++) {
+    heart[i].addEventListener('click', (e) => {
+      currentStatus = !currentStatus;
+      mimicServerCall()
+      .then(() => {
+        if (currentStatus) {
+          e.target.innerText = FULL_HEART;
+          e.target.setAttribute('class', 'like-glyph activated-heart');
+        } else {
+          e.target.innerText = EMPTY_HEART;
+          e.target.setAttribute('class', 'like-glyph')
+        };
+      })
+      .catch((error) => {
+        errorModal.removeAttribute('class');
+        const errorModalMessage = document.querySelector('#modal-message');
+        errorModalMessage.innerText = error;
+        setTimeout(() => {
+          errorModal.setAttribute('class', 'hidden');
+        }, 3000);
+      })
+    });
+  };
+});
 
 
 
